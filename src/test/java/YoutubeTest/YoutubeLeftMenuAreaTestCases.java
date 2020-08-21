9c65d8217ab7a1a2f2a2ee596bb0010c205b65bf
Luis Antonio <9704777+Antoreich@users.noreply.github.com>
2020-08-21 01:21:54 -0500

    Enhanced YoutubeLeftMenuAreaTestCases -> Added @BeforeClass -> Added @BeforeMethod -> Removed unused old lines of code.
    Signed-off-by: Luis Antonio <9704777+Antoreich@users.noreply.github.com>

--- src/test/java/YoutubeTest/YoutubeLeftMenuAreaTestCases.java
+++ src/test/java/YoutubeTest/YoutubeLeftMenuAreaTestCases.java
@@ -1,43 +1,49 @@
 package YoutubeTest;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utils.PropertyReader;
+import youtube.page.YoutubeHomePage;
 import youtube.usersteps.YoutubeHomePageUserSteps;
 
 public class YoutubeLeftMenuAreaTestCases extends BaseTestCase {
+    private YoutubeHomePageUserSteps youtubeHomePageUserSteps;
+
+    @BeforeClass
+    public void beforeClass(){
+        this.youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
+    }
+
     @Test
     public void validateLeftMenuHomeButton(){
-        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
-        YoutubeHomePageUserSteps youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
         youtubeHomePageUserSteps.clickOnLeftMenuHomeButton();
         Assert.assertEquals(this.myDriver.getCurrentUrl(),PropertyReader.getProperty("test.properties","URL"));
     }
     @Test
     public void validateLeftMenuTrendingButton(){
-        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
-        YoutubeHomePageUserSteps youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
         youtubeHomePageUserSteps.clickOnLeftMenuTrendingButton();
         Assert.assertEquals(this.myDriver.getCurrentUrl(),"https://www.youtube.com/feed/trending");
     }
     @Test
     public void validateLeftMenuSubscriptionsButton(){
-        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
-        YoutubeHomePageUserSteps youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
         youtubeHomePageUserSteps.clickOnLeftMenuSubscriptionsButton();
         Assert.assertEquals(this.myDriver.getCurrentUrl(),"https://www.youtube.com/feed/subscriptions");
     }
     @Test
     public void validateLeftMenuLibraryButton(){
-        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
-        YoutubeHomePageUserSteps youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
         youtubeHomePageUserSteps.clickOnLeftMenuLibraryButton();
         Assert.assertEquals(this.myDriver.getCurrentUrl(),"https://www.youtube.com/feed/library");
     }
     @Test
     public void validateLeftMenuHistoryButton(){
-        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
-        YoutubeHomePageUserSteps youtubeHomePageUserSteps = new YoutubeHomePageUserSteps(this.myDriver);
         youtubeHomePageUserSteps.clickOnLeftMenuHistoryButton();
         Assert.assertEquals(this.myDriver.getCurrentUrl(),"https://www.youtube.com/feed/history");
     }
