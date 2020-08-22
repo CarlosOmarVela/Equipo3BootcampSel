87c454278ff0b12166505ddcd2aaced23b838ba6
Luis Antonio <9704777+Antoreich@users.noreply.github.com>
2020-08-22 02:40:35 -0500

    Added new empty method test cases for YoutubeVideoPageUserSteps -> validateHeader -> validateRelatedContent -> validateVideoPlayer -> validateVideoInformation -> validateComments
    Signed-off-by: Luis Antonio <9704777+Antoreich@users.noreply.github.com>

--- src/main/java/youtube/usersteps/YoutubeVideoPageUserSteps.java
+++ src/main/java/youtube/usersteps/YoutubeVideoPageUserSteps.java
@@ -1,7 +1,15 @@
 package youtube.usersteps;
 
+import org.openqa.selenium.WebDriver;
+import youtube.page.YoutubeHomePage;
 import youtube.page.YoutubeVideoPage;
 
 public class YoutubeVideoPageUserSteps {
+
     private YoutubeVideoPage youtubeVideoPage;
+
+    public YoutubeVideoPageUserSteps(WebDriver driver){
+        this.youtubeVideoPage = new YoutubeVideoPage(driver);
+    }
+
 }
--- src/test/java/YoutubeTest/YoutubeVideoDetailsPageTestCases.java
+++ src/test/java/YoutubeTest/YoutubeVideoDetailsPageTestCases.java
@@ -1,4 +1,40 @@
 package YoutubeTest;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import utils.PropertyReader;
+import utils.listeners.TestListener;
+import youtube.usersteps.YoutubeVideoPageUserSteps;
+
+@Listeners({ TestListener.class})
 public class YoutubeVideoDetailsPageTestCases extends BaseTestCase {
+
+    private YoutubeVideoPageUserSteps youtubeVideoPageUserSteps;
+
+    @BeforeClass
+    public void beforeClass(){
+        this.youtubeVideoPageUserSteps = new YoutubeVideoPageUserSteps(this.myDriver);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        this.myDriver.get(PropertyReader.getProperty("test.properties","URL"));
+    }
+
+    @Test
+    public void validateHeader(){}
+
+    @Test
+    public void validateRelatedContent(){}
+
+    @Test
+    public void validateVideoPlayer(){}
+
+    @Test
+    public void validateVideoInformation(){}
+
+    @Test
+    public void validateComments(){}
 }
